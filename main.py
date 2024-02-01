26c683a282bb21217514a3ea89bd4c924965d3f1
Sosou222 <szym.baran222@gmail.com>
2024-02-01 20:29:06 +0100

    added final code

--- main.py
+++ main.py
@@ -13,13 +13,20 @@ def get_letter_count(text):
     return out
 
 def main():
-    print("hello world")\
-
+    #print("hello world")\
+    print("--- Begin report of books/frankenstein.txt ---")
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        #print(file_contents)
         count = get_word_count(file_contents)
         print(f"Word  count:{count}")
+        print("")
         words = get_letter_count(file_contents)
-        print(f"{words}")
+        #print(f"{words}")
+        l = list(words.items())
+        sorted_words = sorted(l,key=lambda x:x[1],reverse = True)
+        for w in sorted_words:
+            if w[0].isalpha():
+                print(f"The '{w[0]}' character was found {w[1]} times")
+    print("--- End report ---")
 main()
